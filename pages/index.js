e072cf59ac81059212b5eb69071035053d5fe893
Jordan Gignac <gignjo16@gmail.com>
2021-08-27 21:44:53 -0400

    Update index

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,78 @@
-import Head from 'next/head'
+import {v4} from 'uuid';
+import React from 'react';
+import Head from 'next/head';
+import TodoItem from '../components/todo-item';
 
 export default function Home() {
+  const [todos, setTodos] = React.useState([]);
+  const [content, setContent] = React.useState('');
+
+  const removeTodo = id => () => {
+    setTodos(todos => todos.filter(i => i.id !== id));
+  };
+
+  const setCompleted = id => completed => {
+    setTodos(todos => todos.map(i => (i.id === id ? {...i, completed} : i)));
+  };
+
+  const addTodo = content => {
+    setTodos(todos => [...todos, {id: v4(), completed: false, content}]);
+    setContent('');
+  };
+
+  const onSubmit = event => {
+    event.preventDefault();
+    addTodo(content);
+  };
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>peer-list</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
         <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
+          peer-<span className="text-primary">list</span>
         </h1>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
+        <code className="mt-3 p-3 font-mono text-lg bg-gray-100 rounded-md">
+          collaborative distributed lists
+        </code>
 
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
+        <form
+          onSubmit={onSubmit}
+          className="mt-6 max-w-2xl form-control w-3/4 mx-auto"
+        >
+          <div className="relative">
+            <input
+              type="text"
+              value={content}
+              onChange={e => setContent(e.target.value)}
+              className="w-full pr-16 input input-primary input-bordered"
+            />
+            <button
+              type="submit"
+              className="absolute top-0 right-0 rounded-l-none btn btn-primary"
+            >
+              Add Item
+            </button>
+          </div>
+        </form>
 
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className="flex flex-col items-center justify-around max-w-4xl mt-6 w-full">
+          {todos.map(({id, completed, content}) => (
+            <TodoItem
+              content={content}
+              completed={completed}
+              removeItem={removeTodo(id)}
+              setCompleted={setCompleted(id)}
+              key={`todo-${id}`}
+            />
+          ))}
         </div>
       </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
     </div>
-  )
+  );
 }
