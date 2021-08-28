9e35dfabbd6420cd83666fed1dddaf6aaf785cc1
Jordan Gignac <gignjo16@gmail.com>
2021-08-28 03:16:07 -0400

    Update index to handle peer events

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,56 @@
 import {v4} from 'uuid';
 import React from 'react';
 import Head from 'next/head';
+import MyPeer from '../utils/my-peer';
 import TodoItem from '../components/todo-item';
 
 export default function Home() {
+  const [peer, setPeer] = React.useState();
   const [todos, setTodos] = React.useState([]);
   const [content, setContent] = React.useState('');
 
-  const removeTodo = id => () => {
+  React.useEffect(() => {
+    import('peerjs').then(({default: Peer}) => {
+      const params = new URLSearchParams(window.location.search);
+      const puid = params.get('puid');
+
+      const myPeer = new MyPeer(Peer, puid, todos);
+
+      myPeer.stream.on('ADD_TODO', data => addTodo(data, false));
+      myPeer.stream.on('REMOVE_TODO', data => removeTodo(data, false));
+      myPeer.stream.on('SET_COMPLETED', data => setCompleted(data, false));
+
+      setPeer(myPeer);
+    });
+  }, []);
+
+  const setCompleted = (id, emit) => {
+    setTodos(todos =>
+      todos.map(i => (i.id === id ? {...i, completed: !i.completed} : i))
+    );
+    if (emit) peer.sendEvent('SET_COMPLETED', id);
+  };
+
+  const removeTodo = (id, emit) => {
     setTodos(todos => todos.filter(i => i.id !== id));
+    if (emit) peer.sendEvent('REMOVE_TODO', id);
   };
 
-  const setCompleted = id => completed => {
-    setTodos(todos => todos.map(i => (i.id === id ? {...i, completed} : i)));
+  const addTodo = (todo, emit) => {
+    setTodos(todos => [...todos, todo]);
+    if (emit) peer.sendEvent('ADD_TODO', todo);
+    if (emit) setContent('');
   };
 
-  const addTodo = content => {
-    setTodos(todos => [...todos, {id: v4(), completed: false, content}]);
-    setContent('');
+  const copyShareLink = () => {
+    const shareString = 'localhost:3000?puid=' + peer.id;
+    navigator.clipboard.writeText(shareString);
   };
 
-  const onSubmit = event => {
+  const onSubmit = (event, emit) => {
+    const todo = {id: v4(), completed: false, content};
     event.preventDefault();
-    addTodo(content);
+    addTodo(todo, emit);
   };
 
   return (
@@ -33,8 +61,15 @@ export default function Home() {
       </Head>
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          peer-<span className="text-primary">list</span>
+        <h1 className="text-6xl font-bold indicator">
+          <div
+            onClick={copyShareLink}
+            className="indicator-item badge badge-secondary cursor-pointer"
+          >
+            share
+          </div>
+          peer-
+          <span className="text-primary">list</span>
         </h1>
 
         <code className="mt-3 p-3 font-mono text-lg bg-gray-100 rounded-md">
@@ -42,7 +77,7 @@ export default function Home() {
         </code>
 
         <form
-          onSubmit={onSubmit}
+          onSubmit={event => onSubmit(event, true)}
           className="mt-6 max-w-2xl form-control w-3/4 mx-auto"
         >
           <div className="relative">
@@ -66,8 +101,8 @@ export default function Home() {
             <TodoItem
               content={content}
               completed={completed}
-              removeItem={removeTodo(id)}
-              setCompleted={setCompleted(id)}
+              removeItem={() => removeTodo(id, true)}
+              setCompleted={() => setCompleted(id, true)}
               key={`todo-${id}`}
             />
           ))}
