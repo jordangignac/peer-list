aa448c9fb9da9ff9584de3343e326528a8071f34
Jordan Gignac <gignjo16@gmail.com>
2021-08-27 21:20:37 -0400

    Add daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   mode: 'jit',
   darkMode: false, // or 'media' or 'class'
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  plugins: [require('daisyui')],
   variants: {extend: {}},
   theme: {extend: {}},
-  plugins: [],
 };
