4a17e90e0d0a96d9f000f30a916a4f3bb2f9cf49
Jordan Gignac <gignjo16@gmail.com>
2021-08-27 21:19:43 -0400

    Prettier base-level files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
 module.exports = {
   mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  variants: {extend: {}},
+  theme: {extend: {}},
   plugins: [],
-}
+};
